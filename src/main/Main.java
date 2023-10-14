f141d5027ddc5fda245d8ac77ae5efa08c32917c
Afzal <109408706+itssoothsayer@users.noreply.github.com>
2023-10-14 17:04:31 +0530

    Add files via upload

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package App;
+import App.sudokuUI; // Replace "yourpackage" with the actual package name of SudokuUI.
+
+
+public class Main {
+    public static  void main(String[] args) {
+        sudokuUI sudokuUI = new sudokuUI();
+        sudokuUI.setVisible(true);
+
+    }
+}
